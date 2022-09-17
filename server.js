63ea2d953a7447b6c7ede788a29b89a384aa7fb9
Sanchayan Das <45485694+sanchayancse@users.noreply.github.com>
2022-09-17 19:45:31 +0530

    initial commit
    Code update for Server setup file

---
+++ server.js
@@ -0,0 +1,56 @@
+import { ApolloServer, gql } from "apollo-server";
+
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
+
+const users = [
+  {
+    id: 1,
+    firstName: "sanchayan",
+    lastName: "das",
+    email: "sanchayandas@gmail.com",
+    password: "12345",
+  },
+  {
+    id: 2,
+    firstName: "munni",
+    lastName: "das",
+    email: "munni@gmail.com",
+    password: "12345",
+  },
+];
+
+const typeDefs = gql`
+  type Query {
+    greet: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greet: () => {
+      return "Hello World";
+    },
+  },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: "bounded",
+  /**
+   * What's up with this embed: true option?
+   * These are our recommended settings for using AS;
+   * they aren't the defaults in AS3 for backwards-compatibility reasons but
+   * will be the defaults in AS4. For production environments, use
+   * ApolloServerPluginLandingPageProductionDefault instead.
+   **/
+  plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+});
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
